35fca11dc1a932f55053cd33ad944c53f15f6e27
Miles Pieterse <milesyusuf123@gmail.com>
2023-09-19 02:58:12 +0200

    Install: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      nichromeFont: ["Nichrome-Regular", "sans"],
+      interFont: ["Inter-Regular", "sans"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
