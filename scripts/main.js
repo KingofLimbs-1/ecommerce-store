41bd466e13680fed47e816e95763b651a629d9c7
KingofLimbs-1 <milesyusuf123@gmail.com>
2023-10-08 16:25:33 +0200

    Update: component rendering functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,24 @@
 // COMPONENTS
 
-// Function to load and render components
-let loadComponent = (componentName, targetElementID) => {
-  fetch(`/components/${componentName}.html`)
-    .then((response) => response.text())
-    .then((html) => {
+document.addEventListener("DOMContentLoaded", async function () {
+  // Function to load and render components
+  const loadComponent = async (componentName, targetElementID) => {
+    try {
+      const response = await fetch(`/components/${componentName}.html`);
+      const html = await response.text();
       const targetElement = document.getElementById(targetElementID);
       if (targetElement) {
         targetElement.innerHTML = html;
       }
-    })
-    .catch((error) => {
+    } catch (error) {
       console.error("Error loading the navbar component", error);
-    });
-};
+    }
+  };
 
-// Function calls
-loadComponent("navbar", "navbar");
-loadComponent("footer", "footer");
+  // Load the navbar and footer components
+  await loadComponent("navbar", "navbar");
+  await loadComponent("footer", "footer");
+  // ...
+});
+
 // ...
