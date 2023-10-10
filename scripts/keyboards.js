bc9d701f8456718e8ae8f9d346f858bae4c88cf7
Miles Pieterse <milesyusuf123@gmail.com>
2023-10-10 12:50:06 +0200

    Add: GET/fetch requests from API endpoints for category specific product data

---
+++ scripts/keyboards.js
@@ -0,0 +1,20 @@
+// GET request for '/api/keyboards' endpoint
+fetch("http://localhost:3000/api/keyboards")
+  .then((response) => {
+    // Check if the response status is OK (200)
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    // Parse the JSON response data
+    return response.json();
+  })
+  .then((data) => {
+    // Interact with product data
+    console.log(data);
+    // ...
+  })
+  .catch((error) => {
+    // Handle any errors that occurred during fetch request
+    console.error("Fetch error:", error);
+  });
+// ...
---
+++ scripts/switches.js
@@ -0,0 +1,20 @@
+// GET request for '/api/switches' endpoint
+fetch("http://localhost:3000/api/switches")
+  .then((response) => {
+    // Check if the response status is OK (200)
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    // Parse the JSON response data
+    return response.json();
+  })
+  .then((data) => {
+    // Interact with product data
+    console.log(data);
+    // ...
+  })
+  .catch((error) => {
+    // Handle any errors that occurred during fetch request
+    console.error("Fetch error:", error);
+  });
+// ...
