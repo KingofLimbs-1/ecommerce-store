584355b6129effbefa202078ef855c9459ead64c
Miles Pieterse <milesyusuf123@gmail.com>
2023-12-24 19:27:59 +0200

    Add: Function that intializes the nav dropdown + more comments for readability

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 // COMPONENTS
-
 document.addEventListener("DOMContentLoaded", async function () {
+  // FUNCTIONS
+
   // Function to load and render components
   const loadComponent = async (componentName, targetElementID) => {
     try {
@@ -14,11 +15,36 @@ document.addEventListener("DOMContentLoaded", async function () {
       console.error("Error loading the navbar component", error);
     }
   };
+  // ...
 
-  // Load the navbar and footer components
+  // Function to initialize the navbar "shop" button dropdown
+  let initializeDropdown = () => {
+    let dropdownButton = document.querySelector("#dropdownButton");
+    let dropdownItems = document.querySelector("#dropdownItems");
+
+    dropdownButton.addEventListener("click", (event) => {
+      // Toggle dropdown visibility
+      if (dropdownItems.classList.contains("hidden")) {
+        dropdownItems.classList.remove("hidden");
+      } else {
+        dropdownItems.classList.add("hidden");
+      }
+
+      // Stop the event from propagating to the document click event
+      event.stopPropagation();
+    });
+  };
+  // ...
+
+  // FUNCTIONS END
+
+  // Load components
   await loadComponent("navbar", "navbar");
   await loadComponent("footer", "footer");
   // ...
-});
 
+  // Run other functionality
+  initializeDropdown();
+  // ...
+});
 // ...
